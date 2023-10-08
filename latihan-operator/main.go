50779cc7cd4a3c19edfb30d6846c475828f16efb
Andra Hotmartua Al Hafiz HSB <52515217+andrahafiz@users.noreply.github.com>
2023-10-08 14:10:37 +0700

    execrise latihan-operator

---
+++ latihan-operator/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var myAge int8 = 20
+	var sisterAge int8 = 30
+
+	fmt.Printf("Umur saya sekarang adalah : %d\n", myAge)
+	fmt.Printf("Umur kakak sekarang adalah : %d\n", sisterAge)
+
+	// myAge++
+	// fmt.Printf("Umur saya tahun depan adalah : %d\n", myAge)
+	// sisterAge++
+	// fmt.Printf("Umur kakak tahun depan adalah : %d\n", sisterAge)
+
+	myAge += 5
+	fmt.Printf("Umur saya 5 tahun lagi adalah : %d\n", myAge)
+	sisterAge += 5
+	fmt.Printf("Umur kakak 5 tahun lagi adalah : %d\n", sisterAge)
+
+}
