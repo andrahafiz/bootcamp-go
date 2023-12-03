a3078f0f690de36a81761f85c1c12f5c4cf4eba1
Andra Hotmartua Al Hafiz HSB <52515217+andrahafiz@users.noreply.github.com>
2023-12-03 13:44:12 +0700

    Sesi 11 - Rest API

---
+++ rest-api/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"rest-api/menu"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	router := chi.NewRouter()
+
+	menu.Register(router, nil)
+
+	const port = ":8000"
+
+	log.Println("server running at port", port)
+
+	http.ListenAndServe(port, router)
+}
